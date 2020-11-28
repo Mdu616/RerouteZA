65dd8dfed7bbe25543fa92897b3a5ddba4bb950d
Mdu616 <55834790+Mdu616@users.noreply.github.com>
2020-11-28 09:00:49 +0200

    Add files via upload

---
+++ build/precache-manifest.2f0b376794c8cdd37eab258cba779108.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "939a0a2257b39b2e31f5e4872703ef3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b426ffc4c96bd2f0335",
+    "url": "/static/css/main.bef43acb.chunk.css"
+  },
+  {
+    "revision": "cddfd08082f2f28533ae",
+    "url": "/static/js/2.126bf728.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.126bf728.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b426ffc4c96bd2f0335",
+    "url": "/static/js/main.6fff9b2e.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
